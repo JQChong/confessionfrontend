37165ed0a9ef331f322f3248270813f1724248b0
Chong Jing Quan <jingquan0302@gmail.com>
2020-12-18 00:44:25 +0800

    edit base.service.ts

--- src/app/model-service/base.service.ts
+++ src/app/model-service/base.service.ts
@@ -9,25 +9,25 @@ export class BaseService {
 
     constructor(private httpClient: HttpClient) { }
 
-    getObjectByStatus(url:string, status: boolean): Observable<any> {
+    getObjectByStatus(url: string, status: boolean): Observable<any> {
         const actualStatus = status ? 'True' : 'False';
         let params = new HttpParams().set('approved', actualStatus);
         return this.httpClient.get(`${url}`, { params });
     }
 
-    createObject(url:string, post: object): Observable<object> {
+    createObject(url: string, post: object): Observable<object> {
         return this.httpClient.post(`${url}`, post);
     }
 
-    approveObject(url:string, id: Number): Observable<any> {
-        return this.httpClient.patch(`${url}/${id}`, { approved: true });
+    approveObject(url: string, id: Number): Observable<any> {
+        return this.httpClient.patch(`${url}/${id}`, { params: { approved: true } });
     }
 
-    updateLikes(url:string, id: number, likes: number): Observable<any> {
-        return this.httpClient.patch(`${url}/${id}`, { likes });
+    updateLikes(url: string, id: number, likes: number): Observable<any> {
+        return this.httpClient.patch(`${url}/${id}`, { params: { likes } });
     }
 
-    deleteObject(url:string, id: Number): Observable<any> {
+    deleteObject(url: string, id: Number): Observable<any> {
         return this.httpClient.delete(`${url}/${id}`);
     }
 }
