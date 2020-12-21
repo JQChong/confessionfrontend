906dfb4fc5243387627c3804f0ca68f1400b1a8e
Chong Jing Quan <jingquan0302@gmail.com>
2020-12-21 23:47:43 +0800

    fix code

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../model-service/post/post.service';
 import { CommentService } from '../model-service/comment/comment.service';
 import { Comment } from '../model-service/comment/comment'
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-page',
@@ -22,32 +22,36 @@ export class PostPageComponent implements OnInit {
    */
 
   post: any; // izit any?
-  comments: any; // izit any?
+  comments: Comment[]; // izit any?
 
   constructor(
     private _postService: PostService,
     private _commentService: CommentService,
+    private route: ActivatedRoute,
     private _router: Router,
   ) { }
 
   ngOnInit(): void {
-    this.post = this._postService.getPostById(this.post.id); // get post id from where?
-    this.comments = this._commentService.getCommentsByPost(this.post.id);
+    this.route.queryParams.subscribe((params: Params) => {
+      const postId = Number(params['id']);
+      this._postService.getPostById(postId).subscribe((post) => { this.post = post; }); // get post id from where?
+      this._commentService.getCommentsByPost(postId).subscribe((comments) => { this.comments = comments.results; });
+    });
   }
 
   goPrevious() {
     let previousId = this.post.id - 1;
-    this._router.navigate(['home/post', previousId]); // is the route correct?
+    this._router.navigate(['home/post'], { queryParams: { id: previousId }}); // is the route correct?
   }
-  
+
   goNext() {
     let nextId = this.post.id + 1;
-    this._router.navigate(['home/post', nextId]); // is the route correct?
+    this._router.navigate(['home/post'], { queryParams: { id: nextId }}); // is the route correct?
   }
 
   updatePostLikes() {
-    this._postService.updateLikes(this.post.id, this.post.likes).subscribe(); // subscribe need para?
-  } 
+    this._postService.updateLikes(this.post.id, this.post.likes + 1).subscribe(); // subscribe need para?
+  }
 
   updateCommentLikes(comment: Comment) {
     this._commentService.updateLikes(comment.id, comment.likes).subscribe(); // subscribe need para?
@@ -55,14 +59,10 @@ export class PostPageComponent implements OnInit {
 
   createNewComment(text: string) {
     const newComment = {
-      id: '',
-      post: this.post,
-      text: text,
-      likes: 0,
-      time_created: '',
-      poster: '',
-      approved: false} // is tis how to create new comment?
+      post: this.post.id,
+      text: text
+    } // is tis how to create new comment?
     this._commentService.createComment(newComment).subscribe(); // subscribe need para?
   }
-  
+
 }
